1332fe62b34ec9ede7987a2be42454d80daaca89
Alexandre Lam <alexandrelam@outlook.com>
2021-12-18 16:34:31 +0100

    feat: :rotating_light: Fetching error handling toast :rotating_light: (#4)

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import IArrow from "../components/icons/IArrow";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Item from "../types/Item";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Home: React.FC = (): React.ReactElement => {
   const [items, setitems] = useState<Array<Item>>([]);
@@ -18,27 +20,41 @@ const Home: React.FC = (): React.ReactElement => {
         setisLoading(false);
       })
       .catch(() => {
-        console.log("fail to fetch");
+        notify();
         setisLoading(false);
       });
   }, []);
 
+  const notify = () =>
+    toast.error("🚨 Failed to fetch!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+    });
+
   return (
-    <div className="flex flex-col items-center gap-20 bg-zinc-100 h-screen p-10">
-      <Dropdown />
-      <div className="flex gap-14">
-        <Card
-          className="self-start"
-          isInventory={true}
-          items={items}
-          isLoading={isLoading}
-        />
-        <IArrow className="-mt-36" />
-        <Card title="Selected" items={[]}>
-          <CardFooter />
-        </Card>
+    <>
+      <ToastContainer />
+
+      <div className="flex flex-col items-center gap-20 bg-zinc-100 h-screen p-10">
+        <Dropdown />
+        <div className="flex gap-14">
+          <Card
+            className="self-start"
+            isInventory={true}
+            items={items}
+            isLoading={isLoading}
+          />
+          <IArrow className="-mt-36" />
+          <Card title="Selected" items={[]}>
+            <CardFooter />
+          </Card>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
