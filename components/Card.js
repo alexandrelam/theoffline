424cb5708887f7b8949af72e50245abc970aceb4
Alexandre Lam <alexandrelam@outlook.com>
2021-12-18 15:45:33 +0100

    feat: cart items

--- components/Card.js
+++ components/Card.js
@@ -1,10 +1,24 @@
-import React, { Children } from "react";
+import React from "react";
+import CardItem from "./CardItem";
 
-export default function Card({ title = "Inventory", className, children }) {
+export default function Card({
+  title = "Inventory",
+  items,
+  isLoading,
+  className,
+  children,
+}) {
   return (
     <div className={`w-80 bg-white rounded-lg border shadow-lg ${className}`}>
       <h2 className="font-bold text-lg border-b text-center py-4">{title}</h2>
-      <div className="h-96"></div>
+      <div className="h-96 p-4 flex flex-col gap-1">
+        {isLoading ? (
+          <span>is loading...</span>
+        ) : (
+          items &&
+          items.map((item, index) => <CardItem key={index} item={item} />)
+        )}
+      </div>
       {children}
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,28 @@ import Dropdown from "../components/Dropdown";
 import Card from "../components/Card";
 import CardFooter from "../components/CardFooter";
 import IArrow from "../components/icons/IArrow";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [items, setitems] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://weekndr.herokuapp.com/api/v2/cabin-luggage-inventory")
+      .then((res) => {
+        setitems(res.data.items);
+        setisLoading(false);
+      })
+      .catch(() => console.log("fail to fetch"));
+  }, []);
+
   return (
     <div className="flex flex-col items-center gap-20 bg-zinc-100 h-screen p-10">
       <Dropdown />
       <div className="flex gap-14">
-        <Card className="self-start" />
+        <Card className="self-start" items={items} isLoading={isLoading} />
         <IArrow className="-mt-36" />
         <Card title="Selected">
           <CardFooter />
