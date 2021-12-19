3d77c4c4664df4f034bea311c2592c052b5ab3ea
Alexandre Lam <alexandrelam@outlook.com>
2021-12-19 20:18:13 +0100

    feat: added total to report page and refacto total weight (#11)

--- components/CardFooterTotal.tsx
+++ components/CardFooterTotal.tsx
@@ -4,12 +4,12 @@ import gram2kilo from "../utils/unitConverter";
 type Props = {
   /**weight displayed in the component */
   totalWeight: number;
-  isWeightExceeded: boolean;
+  isWeightExceeded?: boolean;
 };
 
 const CardFooterTotal: React.FC<Props> = ({
   totalWeight,
-  isWeightExceeded,
+  isWeightExceeded = false,
 }): React.ReactElement => (
   <div className="flex justify-between border-y px-3 py-4">
     <span>Total</span>
